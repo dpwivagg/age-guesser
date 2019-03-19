07d71949aaed0313128a6ec2ba77c024536c869a
Daniel Wivagg <dpwivagg@wpi.edu>
2019-03-19 18:51:25 -0400

    Add model compile

--- predict.py
+++ predict.py
@@ -1,9 +1,14 @@
 import pretrained
 import numpy as np
+from keras import optimizers
 
 def predict(weights_path, faces_path, ages_path=None):
     model = pretrained.create_cnn_model()
     model.load_weights(weights_path)
+    adam = optimizers.adam(lr=0.001)
+    model.compile(loss='mean_squared_error',
+                  optimizer=adam,
+                  metrics=['mean_squared_error'])
     faces = np.load(faces_path)
     faces = np.expand_dims(faces, axis=3)
 
