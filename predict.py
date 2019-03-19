3b059a9aec9b816990760db6637f89d140314fc0
Daniel Wivagg <dpwivagg@wpi.edu>
2019-03-19 19:58:38 -0400

    Delete ineffective model and change to model loading for prediction

--- predict.py
+++ predict.py
@@ -1,10 +1,12 @@
 import pretrained
 import numpy as np
 from keras import optimizers
+from keras.models import load_model
 
 def predict(weights_path, faces_path, ages_path=None):
-    model = pretrained.create_cnn_model()
-    model.load_weights(weights_path)
+    # model = pretrained.create_cnn_model()
+    # model.load_weights(weights_path)
+    model = load_model(weights_path)
     adam = optimizers.adam(lr=0.001)
     model.compile(loss='mean_squared_error',
                   optimizer=adam,
@@ -12,9 +14,11 @@ def predict(weights_path, faces_path, ages_path=None):
     faces = np.load(faces_path)
     faces = np.expand_dims(faces, axis=3)
 
-    prediction = model.predict_classes(faces)
+    prediction = model.predict(faces)
 
     if ages_path:
         ages = np.load(ages_path)
         print(model.evaluate(faces, ages))
         print(model.metrics_names)
+
+    return prediction
